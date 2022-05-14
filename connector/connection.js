8b0eccf0cbf787fc9e44bd43457cae135bf78762
HANSEN EMANUEL <hansenemanuel@hotmail.com>
2022-05-14 20:07:19 +0700

    change var

--- connector/connection.js
+++ connector/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 async function connectMongoose() {
-	await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology:true }).then(() =>{
+	await mongoose.connect("mongodb+srv://gasBali:D8BpxTAeDhnkAQwr@cluster0.uho4h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology:true }).then(() =>{
 		console.log("mongoose connected..")
 	})
 }
