ca4a6dea7a6a87eeeea756eb4962c841a3560a68
HANSEN EMANUEL <hansenemanuel@hotmail.com>
2022-05-16 22:41:56 +0700

    add cors policy

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const userService = require('./service/service.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    });
+
+    next();
+});
+
 app.use('/api/', Routes.Routes());
 
 //app
